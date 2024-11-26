203a34542f6e39525fa98224a12b45865217d188
cristianobeimar <cristianyalande63@gmail.com>
2024-11-26 14:06:44 -0500

    [FEATURE] add component

--- src/components/InfoPersona.jsx
+++ src/components/InfoPersona.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
 
 export const InfoPersona = () => {
   return (
-    <div>InfoPersona</div>
-  )
-}
+    <>
+      <h1>persona</h1>
+      <p>
+        Ingresa la informacion de la persona, conocerla es inportante para hacer
+        una propuesta acorde a sus necesidades
+      </p>
+      <a href="http://">los campos con (*) son obligatorios</a>
+      <div>
+        <div>
+          <img src="Grupo7616.svg" alt="" />
+          <p>
+            <d>Tipo de documento</d>
+          </p>
+          <p>Cedula de Ciudadania</p>
+          <img src="Grupo2046.svg" alt="" />
+          <p>
+            <d>Identificación</d>
+          </p>
+          <p>123456789</p>
+        </div>
+      </div>
+    </>
+  );
+};
