127dd3c853f18778b5ab5fb1980876f0373381b5
Eider4 <167330642+Eider4@users.noreply.github.com>
2024-11-27 09:53:45 -0500

    [FEATURE] add part of the quote page what are the plans

---
+++ src/components/molecules/tables-plans/TablesPlans.jsx
@@ -0,0 +1,38 @@
+import styles from "./styles.module.css";
+import img_financing from "../../../assets/svg/CombinedShape.svg";
+import img_arrow_down from "../../../assets/svg/Icon-Arrowdown.svg";
+export default function TablesPlans() {
+  return (
+    <div className={styles.div_root}>
+      <img className={styles.img_arrow_down} src={img_arrow_down} alt="" />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+        className={styles.div_inf}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "10px",
+          }}
+        >
+          <img className={styles.financing_icon} src={img_financing} />
+          <p className={styles.p_inf}>
+            En este plan no están seleccionadas las siguientes coberturas,
+            puedes agregarlas de manera opcional:
+          </p>
+        </div>
+        <p className={styles.p_inf_2}>
+          - Pérdida parcial por daños y Pérdida parcial por hurto
+        </p>
+      </div>
+      <div></div>
+    </div>
+  );
+}
