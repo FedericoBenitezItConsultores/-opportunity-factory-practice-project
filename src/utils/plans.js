d315c981bd9bf8ec8ca42ba0c741cc07b360b90b
Eider4 <167330642+Eider4@users.noreply.github.com>
2024-11-27 17:14:04 -0500

    [FEATOR]

--- src/utils/plans.js
+++ src/utils/plans.js
@@ -11,6 +11,67 @@ export const plans = [
       color: "#95DFE4",
       height: 10,
     },
+    coberturas: [
+      {
+        cobertura: "Responsabilidad Civil Extracontractual",
+        capital: [3000000000],
+        deducibles: 0,
+      },
+      {
+        cobertura: "Pérdida total por hurto",
+        capital: 28500000,
+        deducibles: 0,
+      },
+      {
+        cobertura: "Pérdida total daños",
+        capital: 28500000,
+        deducibles: 0,
+      },
+      {
+        cobertura: "Pérdida parcial daños",
+        capital: 28500000,
+        deducibles: [950000],
+      },
+      {
+        cobertura: "Pérdida parcial hurto",
+        capital: 28500000,
+        deducibles: [950000],
+      },
+      {
+        cobertura: "Terremoto",
+        capital: 28500000,
+        deducibles: 950000,
+      },
+      {
+        cobertura: "Amparo patrimonial",
+        capital: null,
+        deducibles: null,
+      },
+      {
+        cobertura: "Gastos de transporte por pérdida total hurto",
+        capital: 1200000,
+        deducibles: null,
+      },
+      {
+        cobertura: "Gastos de transporte por pérdida total daños",
+        capital: 1200000,
+        deducibles: null,
+      },
+    ],
+    asistencias: [
+      {
+        asisitecia: "Asistencia jurídica penal",
+        capital: 13385074,
+      },
+      {
+        asisitecia: "Asistencia jurídica civil",
+        capital: 6770.66,
+      },
+      {
+        asisitecia: "Asistencia en viaje",
+        capital: ["Tradicional"],
+      },
+    ],
   },
   {
     id: 2,
@@ -24,6 +85,67 @@ export const plans = [
       color: "#FFD000",
       height: 15,
     },
+    coberturas: [
+      {
+        cobertura: "Responsabilidad Civil Extracontractual",
+        capital: [3000000000],
+        deducibles: 0,
+      },
+      {
+        cobertura: "Pérdida total por hurto",
+        capital: 28500000,
+        deducibles: 0,
+      },
+      {
+        cobertura: "Pérdida total daños",
+        capital: 28500000,
+        deducibles: 0,
+      },
+      {
+        cobertura: "Pérdida parcial daños",
+        capital: 28500000,
+        deducibles: [950000],
+      },
+      {
+        cobertura: "Pérdida parcial hurto",
+        capital: 28500000,
+        deducibles: [950000],
+      },
+      {
+        cobertura: "Terremoto",
+        capital: 28500000,
+        deducibles: 950000,
+      },
+      {
+        cobertura: "Amparo patrimonial",
+        capital: null,
+        deducibles: null,
+      },
+      {
+        cobertura: "Gastos de transporte por pérdida total hurto",
+        capital: 1200000,
+        deducibles: null,
+      },
+      {
+        cobertura: "Gastos de transporte por pérdida total daños",
+        capital: 1200000,
+        deducibles: null,
+      },
+    ],
+    asistencias: [
+      {
+        asisitecia: "Asistencia jurídica penal",
+        capital: 13385074,
+      },
+      {
+        asisitecia: "Asistencia jurídica civil",
+        capital: 6770.66,
+      },
+      {
+        asisitecia: "Asistencia en viaje",
+        capital: ["Tradicional"],
+      },
+    ],
   },
   {
     id: 3,
@@ -37,6 +159,67 @@ export const plans = [
       color: "#191445",
       height: 10,
     },
+    coberturas: [
+      {
+        cobertura: "Responsabilidad Civil Extracontractual",
+        capital: [3000000000],
+        deducibles: 0,
+      },
+      {
+        cobertura: "Pérdida total por hurto",
+        capital: 28500000,
+        deducibles: 0,
+      },
+      {
+        cobertura: "Pérdida total daños",
+        capital: 28500000,
+        deducibles: 0,
+      },
+      {
+        cobertura: "Pérdida parcial daños",
+        capital: 28500000,
+        deducibles: [950000],
+      },
+      {
+        cobertura: "Pérdida parcial hurto",
+        capital: 28500000,
+        deducibles: [950000],
+      },
+      {
+        cobertura: "Terremoto",
+        capital: 28500000,
+        deducibles: 950000,
+      },
+      {
+        cobertura: "Amparo patrimonial",
+        capital: null,
+        deducibles: null,
+      },
+      {
+        cobertura: "Gastos de transporte por pérdida total hurto",
+        capital: 1200000,
+        deducibles: null,
+      },
+      {
+        cobertura: "Gastos de transporte por pérdida total daños",
+        capital: 1200000,
+        deducibles: null,
+      },
+    ],
+    asistencias: [
+      {
+        asisitecia: "Asistencia jurídica penal",
+        capital: 13385074,
+      },
+      {
+        asisitecia: "Asistencia jurídica civil",
+        capital: 6770.66,
+      },
+      {
+        asisitecia: "Asistencia en viaje",
+        capital: ["Tradicional"],
+      },
+    ],
   },
   {
     id: 4,
@@ -50,5 +233,66 @@ export const plans = [
       color: "#28A3AF",
       height: 10,
     },
+    coberturas: [
+      {
+        cobertura: "Responsabilidad Civil Extracontractual",
+        capital: [3000000000],
+        deducibles: 0,
+      },
+      {
+        cobertura: "Pérdida total por hurto",
+        capital: 28500000,
+        deducibles: 0,
+      },
+      {
+        cobertura: "Pérdida total daños",
+        capital: 28500000,
+        deducibles: 0,
+      },
+      {
+        cobertura: "Pérdida parcial daños",
+        capital: 28500000,
+        deducibles: [950000],
+      },
+      {
+        cobertura: "Pérdida parcial hurto",
+        capital: 28500000,
+        deducibles: [950000],
+      },
+      {
+        cobertura: "Terremoto",
+        capital: 28500000,
+        deducibles: 950000,
+      },
+      {
+        cobertura: "Amparo patrimonial",
+        capital: null,
+        deducibles: null,
+      },
+      {
+        cobertura: "Gastos de transporte por pérdida total hurto",
+        capital: 1200000,
+        deducibles: null,
+      },
+      {
+        cobertura: "Gastos de transporte por pérdida total daños",
+        capital: 1200000,
+        deducibles: null,
+      },
+    ],
+    asistencias: [
+      {
+        asisitecia: "Asistencia jurídica penal",
+        capital: 13385074,
+      },
+      {
+        asisitecia: "Asistencia jurídica civil",
+        capital: 6770.66,
+      },
+      {
+        asisitecia: "Asistencia en viaje",
+        capital: ["Tradicional"],
+      },
+    ],
   },
 ];
