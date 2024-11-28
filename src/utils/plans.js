5c8c19195cb31ec91bccde854aadf95867c431c2
Eider4 <167330642+Eider4@users.noreply.github.com>
2024-11-28 11:15:37 -0500

    [FEATURE] configure plan properties

--- src/utils/plans.js
+++ src/utils/plans.js
@@ -13,46 +13,55 @@ export const plans = [
     },
     coberturas: [
       {
+        typeCheck: "icon_done",
         cobertura: "Responsabilidad Civil Extracontractual",
         capital: [3000000000],
         deducibles: 0,
       },
       {
+        typeCheck: "icon_done",
         cobertura: "Pérdida total por hurto",
         capital: 28500000,
         deducibles: 0,
       },
       {
+        typeCheck: "icon_done",
         cobertura: "Pérdida total daños",
         capital: 28500000,
         deducibles: 0,
       },
       {
+        typeCheck: "icon_done",
         cobertura: "Pérdida parcial daños",
         capital: 28500000,
         deducibles: [950000],
       },
       {
+        typeCheck: "icon_done",
         cobertura: "Pérdida parcial hurto",
         capital: 28500000,
         deducibles: [950000],
       },
       {
+        typeCheck: "icon_done",
         cobertura: "Terremoto",
         capital: 28500000,
         deducibles: 950000,
       },
       {
+        typeCheck: "icon_done",
         cobertura: "Amparo patrimonial",
         capital: null,
         deducibles: null,
       },
       {
+        typeCheck: "icon_done",
         cobertura: "Gastos de transporte por pérdida total hurto",
         capital: 1200000,
         deducibles: null,
       },
       {
+        typeCheck: "icon_done",
         cobertura: "Gastos de transporte por pérdida total daños",
         capital: 1200000,
         deducibles: null,
@@ -60,14 +69,17 @@ export const plans = [
     ],
     asistencias: [
       {
+        typeCheck: "icon_done",
         asisitecia: "Asistencia jurídica penal",
         capital: 13385074,
       },
       {
+        typeCheck: "icon_done",
         asisitecia: "Asistencia jurídica civil",
         capital: 6770.66,
       },
       {
+        typeCheck: "icon_done",
         asisitecia: "Asistencia en viaje",
         capital: ["Tradicional"],
       },
@@ -87,66 +99,79 @@ export const plans = [
     },
     coberturas: [
       {
+        typeCheck: "icon_done",
         cobertura: "Responsabilidad Civil Extracontractual",
         capital: [3000000000],
         deducibles: 0,
       },
       {
+        typeCheck: "icon_done",
         cobertura: "Pérdida total por hurto",
         capital: 28500000,
         deducibles: 0,
       },
       {
+        typeCheck: "icon_done",
         cobertura: "Pérdida total daños",
         capital: 28500000,
         deducibles: 0,
       },
       {
+        typeCheck: "icon_done",
         cobertura: "Pérdida parcial daños",
         capital: 28500000,
         deducibles: [950000],
       },
       {
+        typeCheck: "icon_done",
         cobertura: "Pérdida parcial hurto",
         capital: 28500000,
         deducibles: [950000],
       },
       {
+        typeCheck: "icon_done",
         cobertura: "Terremoto",
         capital: 28500000,
         deducibles: 950000,
       },
       {
+        typeCheck: "icon_done",
         cobertura: "Amparo patrimonial",
         capital: null,
         deducibles: null,
       },
       {
+        typeCheck: "icon_done",
         cobertura: "Gastos de transporte por pérdida total hurto",
         capital: 1200000,
         deducibles: null,
       },
       {
+        typeCheck: "icon_done",
         cobertura: "Gastos de transporte por pérdida total daños",
         capital: 1200000,
         deducibles: null,
       },
       {
+        typeCheck: "icon_done",
         cobertura: "Vehículo Sustituto",
         capital: null,
         deducibles: null,
       },
       {
+        typeCheck: "icon_done",
         cobertura: "Accidentes personales",
         capital: 40000000,
         deducibles: null,
       },
       {
+        typeCheck: "icon_done",
         cobertura: "RC General Familiar",
         capital: 44000000,
         deducibles: 950000,
       },
       {
+        typeCheck: "square",
         cobertura: "Cobertura de vidrios",
         capital: null,
         deducibles: null,
@@ -154,26 +179,32 @@ export const plans = [
     ],
     asistencias: [
       {
+        typeCheck: "icon_done",
         asisitecia: "Asistencia jurídica penal",
         capital: 13385074,
       },
       {
+        typeCheck: "icon_done",
         asisitecia: "Asistencia jurídica civil",
         capital: 6770.66,
       },
       {
+        typeCheck: "icon_done",
         asisitecia: "Asistencia en viaje",
         capital: ["Tradicional"],
       },
       {
+        typeCheck: "icon_done",
         asisitecia: "Asistencia odontológica autos",
         capital: null,
       },
       {
+        typeCheck: "icon_done",
         asisitecia: "Exequial",
         capital: null,
       },
       {
+        typeCheck: "icon_done",
         asisitecia: "Trámite de Tránsito",
         capital: null,
       },
@@ -193,66 +224,79 @@ export const plans = [
     },
     coberturas: [
       {
+        typeCheck: "icon_done",
         cobertura: "Responsabilidad Civil Extracontractual",
         capital: [3000000000],
         deducibles: 0,
       },
       {
+        typeCheck: "square",
         cobertura: "Pérdida total por hurto",
         capital: 28500000,
         deducibles: 0,
       },
       {
+        typeCheck: "square",
         cobertura: "Pérdida total daños",
         capital: 28500000,
         deducibles: 0,
       },
       {
+        typeCheck: "square",
         cobertura: "Pérdida parcial daños",
         capital: 28500000,
         deducibles: [950000],
       },
       {
+        typeCheck: "square",
         cobertura: "Pérdida parcial hurto",
         capital: 28500000,
         deducibles: [950000],
       },
       {
+        typeCheck: "square",
         cobertura: "Terremoto",
         capital: 28500000,
         deducibles: 950000,
       },
       {
+        typeCheck: "icon_done",
         cobertura: "Amparo patrimonial",
         capital: null,
         deducibles: null,
       },
       {
+        typeCheck: "square",
         cobertura: "Gastos de transporte por pérdida total hurto",
         capital: 1200000,
         deducibles: null,
       },
       {
+        typeCheck: "icon_done",
         cobertura: "Gastos de transporte por pérdida total daños",
         capital: 1200000,
         deducibles: null,
       },
       {
+        typeCheck: "square",
         cobertura: "Vehículo Sustituto",
         capital: null,
         deducibles: null,
       },
       {
+        typeCheck: "square",
         cobertura: "Accidentes personales",
         capital: 40000000,
         deducibles: null,
       },
       {
+        typeCheck: "square",
         cobertura: "RC General Familiar",
         capital: 44000000,
         deducibles: 950000,
       },
       {
+        typeCheck: "square",
         cobertura: "Cobertura de vidrios",
         capital: null,
         deducibles: null,
@@ -260,31 +304,38 @@ export const plans = [
     ],
     asistencias: [
       {
+        typeCheck: "icon_done",
         asisitecia: "Asistencia jurídica penal",
         capital: 13385074,
       },
       {
+        typeCheck: "icon_done",
         asisitecia: "Asistencia jurídica civil",
         capital: 6770.66,
       },
       {
+        typeCheck: "icon_done",
         asisitecia: "Asistencia en viaje",
         capital: ["Tradicional"],
       },
       {
+        typeCheck: "square",
         asisitecia: "Asistencia odontológica autos",
         capital: null,
       },
       {
+        typeCheck: "square",
         asisitecia: "Exequial",
         capital: null,
       },
       {
+        typeCheck: "square",
         asisitecia: "Trámite de Tránsito",
         capital: null,
       },
     ],
   },
+  // falta los iconos de este
   {
     id: 4,
     name: "Ligero",
@@ -299,93 +350,119 @@ export const plans = [
     },
     coberturas: [
       {
+        typeCheck: "icon_done",
         cobertura: "Responsabilidad Civil Extracontractual",
         capital: [3000000000],
         deducibles: 0,
       },
       {
+        typeCheck: "icon_done",
         cobertura: "Pérdida total por hurto",
         capital: 28500000,
         deducibles: 0,
       },
       {
+        typeCheck: "icon_done",
         cobertura: "Pérdida total daños",
         capital: 28500000,
         deducibles: 0,
       },
       {
+        typeCheck: "icon_done",
         cobertura: "Pérdida parcial daños",
         capital: 28500000,
         deducibles: [950000],
       },
       {
+        typeCheck: "icon_done",
         cobertura: "Pérdida parcial hurto",
         capital: 28500000,
         deducibles: [950000],
       },
       {
+        typeCheck: "icon_done",
         cobertura: "Terremoto",
         capital: 28500000,
         deducibles: 950000,
       },
       {
+        typeCheck: "icon_done",
         cobertura: "Amparo patrimonial",
         capital: null,
         deducibles: null,
       },
       {
+        typeCheck: "icon_done",
         cobertura: "Gastos de transporte por pérdida total hurto",
         capital: 1200000,
         deducibles: null,
       },
       {
+        typeCheck: "icon_done",
         cobertura: "Gastos de transporte por pérdida total daños",
         capital: 1200000,
         deducibles: null,
       },
       {
+        typeCheck: "icon_done",
         cobertura: "Gastos de transporte por pérdida total daños",
         capital: 1200000,
         deducibles: null,
       },
       {
+        typeCheck: "icon_done",
         cobertura: "Vehículo Sustituto",
         capital: null,
         deducibles: null,
       },
       {
+        typeCheck: "icon_done",
         cobertura: "Accidentes personales",
         capital: 13385000,
         deducibles: null,
       },
       {
+        typeCheck: "icon_done",
         cobertura: "RC General Familiar",
+        capital: 6770660,
+        deducibles: null,
+      },
+      ,
+      {
+        typeCheck: "square",
+        cobertura: "Cobertura de vidrios",
         capital: 6770660,
         deducibles: null,
       },
     ],
     asistencias: [
       {
+        typeCheck: "icon_done",
         asisitecia: "Asistencia jurídica penal",
         capital: 13385074,
       },
       {
+        typeCheck: "icon_done",
         asisitecia: "Asistencia jurídica civil",
         capital: 6770.66,
       },
       {
+        typeCheck: "icon_done",
         asisitecia: "Asistencia en viaje",
         capital: ["Tradicional"],
       },
       {
+        typeCheck: "icon_done",
         asisitecia: "Asistencia odontológica autos",
         capital: null,
       },
       {
+        typeCheck: "icon_done",
         asisitecia: "Exequial",
         capital: null,
       },
       {
+        typeCheck: "icon_done",
         asisitecia: "Trámite de Tránsito",
         capital: null,
       },
