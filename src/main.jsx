d97d9ab7f0c2d6560ebe6d330012b98bb5579ef5
Eider4 <167330642+Eider4@users.noreply.github.com>
2024-11-29 15:38:10 -0500

    [FEATURE] Configuration for when the plan table has no attributes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import AppRouter from './routes/Router.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./src/App";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AppRouter />
-  </StrictMode>,
-)
+    <App />
+  </StrictMode>
+);
