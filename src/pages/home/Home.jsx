67fc690fba48a0434a64151a4bd2ab139934f925
Eider4 <167330642+Eider4@users.noreply.github.com>
2024-11-30 16:00:11 -0500

    [FEATURE] add maria's changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import Navbar from "../../components/organisms/navbar-2/Navbar";
 import Poput from "../../components/organisms/poput/Poput";
-import PoputReload from "../../components/molecules/poput-reload2/PoputReload";
 import PoputWatingTime from "../../components/molecules/popup-wating-time/PoputWatingTime";
 import WatingTime from "../../components/molecules/popup-wating-time/WatingTime";
+import PoputReloadContainer from "../../components/molecules/poput-reload/PoputReloadContainer";
 
 const Home = () => {
   const [popup, setPopup] = useState(false);
@@ -11,10 +11,10 @@ const Home = () => {
     <div>
       {/* <h1>Home</h1> */}
       <Navbar />
-      {/* <Poput>
-        <PoputReload continuarCotizacion={true} btnReintentar={true} />
-      </Poput>{" "} */}
-      <WatingTime />
+      <Poput>
+        <PoputReloadContainer />
+      </Poput>
+      {/* <WatingTime /> */}
     </div>
   );
 };
