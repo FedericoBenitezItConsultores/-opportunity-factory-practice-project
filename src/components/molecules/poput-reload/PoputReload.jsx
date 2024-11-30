99b8f254f684ef8e0b7531b80a228d6216aa8c17
Eider4 <167330642+Eider4@users.noreply.github.com>
2024-11-30 09:17:43 -0500

    [FEATURE] primary popup

---
+++ src/components/molecules/poput-reload/PoputReload.jsx
@@ -0,0 +1,32 @@
+import styles from "./styles.module.css";
+//  className={styles.div_root}
+export default function PoputReload({
+  continarCotizacion: continuarCotizacion = true,
+}) {
+  return (
+    <div className={styles.div_root}>
+      <p className={styles.title}>¡Ups! Lo sentimos</p>
+      <p className={styles.text1}>
+        {continuarCotizacion
+          ? "No podemos continuar con la cotización en este momento debido a problemas técnicos."
+          : "No podemos generar el documento de la cotización en este momento debido a problemas técnicos."}
+      </p>
+      <p className={styles.text1}>
+        Por favor, intenta nuevamente a través del botón Reintentar. Si el error
+        persiste, retoma la cotización en unos minutos desde Mis Negocios.
+      </p>
+      <button
+        onClick={() => console.log("continuar")}
+        className={styles.button}
+      >
+        Continuar
+      </button>
+      <button
+        onClick={() => console.log("continuar")}
+        className={styles.button}
+      >
+        Continuar
+      </button>
+    </div>
+  );
+}
