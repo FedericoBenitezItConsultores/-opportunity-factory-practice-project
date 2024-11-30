5e5217fe97396d5be9b30a1e4fb479428634e938
Eider4 <167330642+Eider4@users.noreply.github.com>
2024-11-30 08:13:11 -0500

    [FEATURE] create the poput file in the organization

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Navbar from "../../components/organisms/navbar-2/Navbar";
+import Poput from "../../components/organisms/poput/Poput";
 
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
       <Navbar />
+      <Poput>
+        <h1></h1>
+      </Poput>
     </div>
   );
 };
