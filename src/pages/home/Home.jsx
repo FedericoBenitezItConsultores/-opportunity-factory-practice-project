258ee9b0321dc285d4829a2e8b11fc85f2dd1cec
Eider4 <167330642+Eider4@users.noreply.github.com>
2024-11-30 09:17:27 -0500

    [FEATURE] primary popup

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Navbar from "../../components/organisms/navbar-2/Navbar";
 import Poput from "../../components/organisms/poput/Poput";
+import PoputReload from "../../components/molecules/poput-reload/PoputReload";
 
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      {/* <h1>Home</h1> */}
       <Navbar />
       <Poput>
-        <h1></h1>
+        <PoputReload />
       </Poput>
     </div>
   );
