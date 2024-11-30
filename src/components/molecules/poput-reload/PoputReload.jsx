03c61d21b822841d365688dfe240d4741837da8c
Eider4 <167330642+Eider4@users.noreply.github.com>
2024-11-30 10:22:50 -0500

    [FEATURE] add styles to popout reload

--- src/components/molecules/poput-reload/PoputReload.jsx
+++ src/components/molecules/poput-reload/PoputReload.jsx
@@ -1,32 +1,50 @@
 import styles from "./styles.module.css";
+import warning from "../../../assets/svg/Grupo5094.svg";
 //  className={styles.div_root}
 export default function PoputReload({
   continarCotizacion: continuarCotizacion = true,
 }) {
   return (
     <div className={styles.div_root}>
+      <img src={warning} />
       <p className={styles.title}>¡Ups! Lo sentimos</p>
-      <p className={styles.text1}>
-        {continuarCotizacion
-          ? "No podemos continuar con la cotización en este momento debido a problemas técnicos."
-          : "No podemos generar el documento de la cotización en este momento debido a problemas técnicos."}
+      <div>
+        <p className={styles.text1}>
+          {continuarCotizacion ? (
+            <>
+              No podemos continuar con la <b>cotización</b> en este momento
+              debido a problemas técnicos.
+            </>
+          ) : (
+            <>
+              No podemos generar el documento de la <b>cotización</b> en este
+              momento debido a problemas técnicos.
+            </>
+          )}
+        </p>
+        <p className={styles.text1}>
+          Por favor, intenta nuevamente a través del botón <b>Reintentar</b>. Si
+          el error persiste, retoma la cotización en unos minutos desde{" "}
+          <b>Mis Negocios.</b>
+        </p>
+      </div>
+      <p className={styles.text_num_quotation}>
+        Número de cotización <span>100030819</span>
       </p>
-      <p className={styles.text1}>
-        Por favor, intenta nuevamente a través del botón Reintentar. Si el error
-        persiste, retoma la cotización en unos minutos desde Mis Negocios.
-      </p>
-      <button
-        onClick={() => console.log("continuar")}
-        className={styles.button}
-      >
-        Continuar
-      </button>
-      <button
-        onClick={() => console.log("continuar")}
-        className={styles.button}
-      >
-        Continuar
-      </button>
+      <div>
+        <button
+          onClick={() => console.log("continuar")}
+          className={styles.button1}
+        >
+          Mis Negocios
+        </button>
+        <button
+          onClick={() => console.log("continuar")}
+          className={styles.button}
+        >
+          Reintentar
+        </button>
+      </div>
     </div>
   );
 }
