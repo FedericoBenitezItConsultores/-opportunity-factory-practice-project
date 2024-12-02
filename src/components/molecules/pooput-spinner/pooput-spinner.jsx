8815a7754b10dfa23339568b984697214e5a5e13
Mariatroyano <maria2020troyano@gmail.com>
2024-12-02 13:31:30 -0500

    [BUGFIX] poput loadading fix

--- src/components/molecules/pooput-spinner/pooput-spinner.jsx
+++ src/components/molecules/pooput-spinner/pooput-spinner.jsx
@@ -2,24 +2,27 @@ import React from "react";
 import styles from "./styles.module.css";
 import Group from "../../../../public/Group.svg";
 import LoadingImg from "../../../../public/LoadingImg.svg";
+import Poput from "../../organisms/poput/Poput";
 const Spinner = ({ showText = false }) => {
   return (
-    <div className={styles.container}>
-      <div className={styles.loading}>
-        <div style={{ position: "relative" }}>
-          <img alt="" src={Group} className={styles.logo} />
-          <img alt="" src={LoadingImg} className={styles.img} />
+    <Poput>
+      <div className={styles.container}>
+        <div className={styles.loading}>
+          <div style={{ position: "relative" }}>
+            <img alt="" src={Group} className={styles.logo} />
+            <img alt="" src={LoadingImg} className={styles.img} />
+          </div>
         </div>
+        {showText && (
+          <div className={styles.parrafos}>
+            <p className={styles.parrafo}>
+              Este proceso puede tardar algunos segundos más,
+              <br /> por favor espera mientras seguimos realizando la consulta.
+            </p>
+          </div>
+        )}
       </div>
-      {!showText && (
-        <div className={styles.parrafos}>
-          <p className={styles.parrafo}>
-            Este proceso puede tardar algunos segundos más,<br/> por favor espera
-            mientras seguimos realizando la consulta.
-          </p>
-        </div>
-      )}
-    </div>
+    </Poput>
   );
 };
 export default Spinner;
