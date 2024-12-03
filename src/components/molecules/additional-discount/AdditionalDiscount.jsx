8380473978ff83c910f22052f9eaa38639586d6a
jhon1253 <sebastianquinayas@gmail.com>
2024-12-02 23:08:40 -0500

    agregar cambios

--- src/components/molecules/additional-discount/AdditionalDiscount.jsx
+++ src/components/molecules/additional-discount/AdditionalDiscount.jsx
@@ -13,11 +13,15 @@ export const AdditionalDiscount = () => {
     setShowPopup(e.target.checked); // Muestra el popup si el checkbox está marcado
   };
   const identidades = [
-    { value: "opcion1", type: "0", label: "Cédula de extranjería" },
-    { value: "opcion2", type: "1", label: "Carnet Diplomático" },
-    { value: "opcion3", type: "2", label: "Pasaporte" },
-    { value: "opcion4", type: "0", label: "Tarjeta de Identidad" },
-    { value: "opcion5", type: "1", label: "Registro Civil" },
+    { value: "opcion1", type: "0", label: "Cédula de ciudadanía" },
+    { value: "opcion2", type: "1", label: "Cédula de extranjería" },
+    { value: "opcion3", type: "2", label: "Carnet Diplomático" },
+    { value: "opcion4", type: "0", label: "Pasaporte" },
+    { value: "opcion5", type: "1", label: "Tarjeta de Identidad" },
+    { value: "opcion6", type: "0", label: "Registro Civil" },
+    { value: "opcion7", type: "1", label: "Permiso Especial de Permanencia" },
+    { value: "opcion8", type: "2", label: "Documento de Identidad Consular" },
+    { value: "opcion9", type: "0", label: "Certificado de Nacimiento" },
     {
       value: "opcion6",
       type: "2",
