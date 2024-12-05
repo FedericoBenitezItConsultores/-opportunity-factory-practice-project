5aaa58d7e0c05991b57431769bc11aef2e449838
Mariatroyano <maria2020troyano@gmail.com>
2024-12-05 15:24:25 -0500

    [FEATUREA add panel-boox]

--- src/routes/configRouter.jsx
+++ src/routes/configRouter.jsx
@@ -1,8 +1,9 @@
+import { PanelInformation } from "../components/collections/panel-box/PanelInformation";
 import PersonPage from "../pages/person/PersonPage";
 import QuotationPage from "../pages/quotation/QuotationPage";
 
 export const routes = [
   { path: "/", element: <PersonPage /> },
   { path: "/quotation", element: <QuotationPage /> },
-
+  
 ];
