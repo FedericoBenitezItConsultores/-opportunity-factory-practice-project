3e9f2b188f6be7578f8a9a27f89adb0d10c9e1ca
yinethriv <karinariv0311@gmail.com>
2024-12-05 17:13:37 -0500

    [FEATURE]reusable pop-up creation

--- src/components/collections/popup/PopupMain.jsx
+++ src/components/collections/popup/PopupMain.jsx
@@ -1,6 +1,78 @@
-import React from "react";
 import Popup from "../../basic/popup/Popup";
+import React, { useState } from "react";
+import styles from "./Popup.module.css";
+import IconDanger from "../../../assets/icons/svg/Icon-danger.svg";
+import FooterPopup from "../../basic/popup/footer-popup/FooterPopup";
+import Button from "../../basic/button/Button";
 
-export default function PopupMain() {
-  return <Popup>Popup</Popup>;
-}
+const ModalBase = ({
+  img = IconDanger,
+  title = <span style={{ color: "#1A1446" }}>¡Ups! Lo sentimos</span>,
+  buttons = [
+    {
+      typeStyle: "primary",
+      text: "Mis negocios",
+      action: console.log("hello soy "),
+    },
+    {
+      typeStyle: "secondary",
+      text: "Reintentar",
+      action: console.log("hello soy "),
+    },
+  ],
+  text1 = (
+    <>
+      No podemos continuar con la cotización en este momento debido a problemas
+      técnicos.
+      <b>Reintentar.</b> <br /> Si el error persiste, retoma la cotización en
+      unos minutos desde <br />
+      <b>Mis Negocios</b>.
+    </>
+  ),
+  text2 = (
+    <>
+      Reporta la falla en la mesa de ayuda al correo{" "}
+      <p>soporte.aplicativos@libertycolombia.com</p>
+    </>
+  ),
+  numQuotation = true,
+}) => {
+  return (
+    <Popup>
+      <div>
+        <div className={styles.modalHeader}>
+          <img className={styles.IconDanger} src={img} alt="Alerta" />
+          <h2 className={styles.modalTitle}>{title}</h2>
+        </div>
+
+        <div className={styles.modalBody}>
+          <div className={styles.container_text}>
+            <p className={styles.modalText}>{text1}</p>
+            <p className={styles.modalText}>{text2}</p>
+          </div>
+          {numQuotation && (
+            <p style={{ marginLeft: "13%" }} className={styles.modalText}>
+              Número de cotización
+              <span className={styles.quoteNumber}>100030819</span>
+            </p>
+          )}
+        </div>
+
+        <div className={styles.container_btns}>
+          {buttons.map((buton, i) => (
+            <Button
+              key={i}
+              onClick={() => buton.action()}
+              typeStyle={buton.typeStyle}
+            >
+              {buton.text}
+            </Button>
+          ))}
+        </div>
+        <FooterPopup />
+      </div>
+    </Popup>
+  );
+};
+
+export default ModalBase;
--- src/pages/person/PersonPage.jsx
+++ src/pages/person/PersonPage.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+// import PopupMain from "../../components/collections/popup/PopupMain";
+import ModalBase from "../../components/collections/popup/PopupMain";
 
 export default function PersonPage() {
-  return <div>PersonPage</div>;
+  return (
+    <div>
+      {/* <PopupMain /> */}
+      <ModalBase/>
+    </div>
+  );
 }
