c10f6c849ee8bb669995d6f6d1eab19764b892da
Mariatroyano <maria2020troyano@gmail.com>
2024-12-05 14:11:02 -0500

    [FEATURE BUG FIXE]

--- src/routes/configRouter.jsx
+++ src/routes/configRouter.jsx
@@ -1,9 +1,8 @@
 import PersonPage from "../pages/person/PersonPage";
-import FormEaseFeedback from "../pages/quotation/collection/form-ease-feedback/FormEaseFeedback";
 import QuotationPage from "../pages/quotation/QuotationPage";
 
 export const routes = [
   { path: "/", element: <PersonPage /> },
   { path: "/quotation", element: <QuotationPage /> },
-  { path: "/FormEaseFeedback", element: <FormEaseFeedback /> },
+
 ];
