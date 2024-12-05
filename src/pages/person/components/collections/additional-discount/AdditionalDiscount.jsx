3dd14e315ca85554de3a4e76a85fa6c8955d758d
Nicol Tapasco <tapasconicol25@gmail.com>
2024-12-05 17:15:20 -0500

    [FEACTURE] additionaldiscount advances with click management

--- src/pages/person/components/collections/additional-discount/AdditionalDiscount.jsx
+++ src/pages/person/components/collections/additional-discount/AdditionalDiscount.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
 import styles from "../additional-discount/AdditionalDiscount.module.css";
 import iconCheck from "../../../../../assets/icons/svg/iconCheck.svg";
+import { PanelInformation } from "../../../../../components/collections/panel-box/PanelInformation";
+import SelectBig from "../../basics/select-big/SelectBig";
+
 const AdditionalDiscount = () => {
   const [checked, setChecked] = useState(false);
+
   return (
     <div className={styles.container}>
       <h3 className={styles.title}>Descuento adicional</h3>
@@ -19,11 +23,32 @@ const AdditionalDiscount = () => {
           Deseo utilizar el descuento de mi cónyuge
         </label>
       </div>
-      {/* {checked && (
+      <div>
+        <PanelInformation
+          parrafo1="El uso del descuento del cónyuge estará sujeto a posteriores revisiones y validaciones"
+          parrafo2={
+            checked
+              ? "Se aplicará siempre el descuento más alto entre conductor y cónyuge."
+              : " "
+          }
+        />
+      </div>
+      {checked && (
         <>
-          <h1>jdsfeshrfeiuh</h1>
+          <div className={styles.container_form}>
+            <SelectBig />
+            <input type="text" />
+          </div>
+          <p className={styles.info}>
+            Ingresa la identificación de tu cónyuge si quieres aplicar su
+            descuento
+          </p>
+          <div className={styles.cuadro}>
+            %<br />
+            El porcentaje de descuento de tu cónyuge es:
+          </div>
         </>
-      )} */}
+      )}
     </div>
   );
 };
