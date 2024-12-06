2f94caeae25f7aa711ff7fa85c994aba91167584
yinethriv <karinariv0311@gmail.com>
2024-12-06 11:14:38 -0500

    [FEATURE]creation WeCanGenerateReportBug

--- src/pages/person/PersonPage.jsx
+++ src/pages/person/PersonPage.jsx
@@ -1,15 +1,7 @@
-import React from "react";
-import ContinueWithQuoteTryLaterDriverDiscount from "../../components/collections/popup/cards-pop-up/ContinueWithQuoteTryLaterDriverDiscount";
-// import PopupMain from "../../components/collections/popup/PopupMain";
+import React from 'react'
 
 export default function PersonPage() {
   return (
-    <div>
-      {/* <PopupMain /> */}
-      <ContinueWithQuoteTryLaterDriverDiscount
-        btn_misNegocios={() => console.log("btn_misNegocioswe")}
-        btn_reintentar={() => console.log("btn_reintentawer")}
-      />
-    </div>
-  );
+    <div>PersonPage</div>
+  )
 }
