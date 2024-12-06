f7c6a09a51fb72186e620be15c2c36db2eb56dcb
Eider4 <167330642+Eider4@users.noreply.github.com>
2024-12-06 08:33:32 -0500

    [FEATURE]merge master with rama-maria

--- src/pages/person/PersonPage.jsx
+++ src/pages/person/PersonPage.jsx
@@ -1,32 +1,9 @@
 import React from "react";
 import CardDiscount from "./components/collections/card-discount/CardDiscount";
-import imgFirst from "../../assets/icons/svg/Group8.svg"
-import imgSecond from "../../assets/icons/svg/Wedding_Yellow.svg"
-import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
-
+import imgFirst from "../../assets/icons/svg/Group8.svg";
+import imgSecond from "../../assets/icons/svg/Wedding_Yellow.svg";
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 
 export default function PersonPage() {
-
-  return (
-    <div>
-      PersonPage
-      <CardDiscount />
-
-      <CardDiscount
-         title={<span style={{ marginLeft: "-30px" }}>Descuento de cónyuge</span>}
-        text1="El porcentaje de descuento es:"
-        imgSrc={imgFirst} 
-        layerImageSrc={imgSecond}
-        style={{ marginLeft: "-370px" }}
-        porcentaje={{
-          text: "30% OFF", 
-          color: "#FF6347", 
-          textDescuento: "No aplica por descuento menor. Se aplica descuento de cónyuge.",
-          bg: "#FFF0E0",
-          icon: CheckCircleOutlineIcon, 
-          colorIcon: "#FF4500", 
-        }}
-      />
-    </div>
-  );
+  return <div>PersonPage</div>;
 }
