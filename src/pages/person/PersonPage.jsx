0234315567c015170518683beb4d3541bf17bfdf
cristianobeimar <cristianyalande63@gmail.com>
2024-12-06 08:30:02 -0500

    mi rama

--- src/pages/person/PersonPage.jsx
+++ src/pages/person/PersonPage.jsx
@@ -4,7 +4,6 @@ import { Input } from "../../components/basic/input/Input";
 export default function PersonPage() {
   return (
     <>
-      <ButtonPerson>Lo que se les de la gana !</ButtonPerson>
       <Input />
     </>
   );
