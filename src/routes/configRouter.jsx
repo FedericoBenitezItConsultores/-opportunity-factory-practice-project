e1e43e5bd4dac5b653229634434e1fcf7c169dfe
yinethriv <karinariv0311@gmail.com>
2024-12-06 11:13:25 -0500

    [FEATURE]creation WeCanGenerateReportBug

--- src/routes/configRouter.jsx
+++ src/routes/configRouter.jsx
@@ -1,4 +1,3 @@
-import { PanelInformation } from "../components/collections/panel-box/PanelInformation";
 import PersonPage from "../pages/person/PersonPage";
 import QuotationPage from "../pages/quotation/QuotationPage";
 import CardPlan from "../pages/quotation/basic/card-plan/CardPlan"
@@ -6,6 +5,6 @@ import CardPlan from "../pages/quotation/basic/card-plan/CardPlan"
 export const routes = [
   { path: "/", element: <PersonPage /> },
   { path: "/quotation", element: <QuotationPage /> },
-  { path: "/CardPlan", element: <CardPlan/> },
-  
+
+  { path: "/CardPlan", element: <CardPlan /> },
 ];
