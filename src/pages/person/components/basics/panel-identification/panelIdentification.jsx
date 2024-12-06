12c8bffb3f1dc1d14b64fea8423eb801bcf2b6b5
Nicol Tapasco <tapasconicol25@gmail.com>
2024-12-06 12:46:09 -0500

    [FEACTURE] PanelIdentification completed

--- src/pages/person/components/basics/panel-identification/panelIdentification.jsx
+++ src/pages/person/components/basics/panel-identification/panelIdentification.jsx
@@ -1,7 +1,51 @@
 import React from 'react'
+import profile from "../../../../../assets/icons/svg/profile.svg"
+import identification from "../../../../../assets/icons/svg/identification.svg"
 
-export default function panelIdentification() {
+import style from "../../../../../pages/person/components/basics/panel-identification/panelIdentification.module.css"
+
+export default function PanelIdentification() {
   return (
-    <div>panelIdentification</div>
+    <>
+    
+    <div id="form" className={style.containerCard}>
+          <div>
+            <table>
+              <tr>
+                <td>
+                  <div className={style.line} />
+                </td>
+                <td>
+                  <img className={style.Image} src={profile} alt="" />
+                </td>
+                <tr>
+                  <td className={style.documents}>Tipo de documento</td>
+                </tr>
+                <tr>
+                  <td>Cédula de Ciudadanía</td>
+                </tr>
+              </tr>
+            </table>
+          </div>
+          <div>
+            <table>
+              <tr>
+                <td>
+                  <div className={style.line} />
+                </td>
+                <td>
+                  <img className={style.Image} src={identification} alt="" />
+                </td>
+                <tr>
+                  <td className={style.documents}>Identificación</td>
+                </tr>
+                <tr>
+                  <td className={style.numberCc}>123456789</td>
+                </tr>
+              </tr>
+            </table>
+          </div>
+        </div>
+    </>
   )
 }
