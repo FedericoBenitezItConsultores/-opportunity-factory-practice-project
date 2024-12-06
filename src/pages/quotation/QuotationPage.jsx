b8fb34863f780daf6d278f443083632716ba9e74
Eider4 <167330642+Eider4@users.noreply.github.com>
2024-12-05 20:33:35 -0500

    [FEATURE] Implement the plan tables, generate-inf-pdf component and footerButtons to the quote page

--- src/pages/quotation/QuotationPage.jsx
+++ src/pages/quotation/QuotationPage.jsx
@@ -1,14 +1,24 @@
+import { useNavigate } from "react-router-dom";
 import FooterButtons from "../../components/collections/footer-buttons/FooterButtons";
 import GenerateInPdf from "./basic/generate-inf-pdf/GenerateInPdf";
 import PanelPlans from "./collection/panel-plans/PanelPlans";
 import styles from "./styles.module.css";
 export default function QuotationPage() {
+  const navigate = useNavigate();
+  const functionVolver = () => navigate("/");
+
+  const functionContinuar = () => {};
+  const functionGuardar = () => {};
   return (
     <div>
       <PanelPlans />
       <GenerateInPdf />
       <div className={styles.container_footer_btns}>
-        <FooterButtons />
+        <FooterButtons
+          functionContinuar={functionContinuar}
+          functionVolver={functionVolver}
+          functionGuardar={functionGuardar}
+        />
       </div>
     </div>
   );
