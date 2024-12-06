c4ef97d5700e241b0dafa7a19c151b9d6cad0c5c
jhon1253 <sebastianquinayas@gmail.com>
2024-12-06 12:36:54 -0500

    [BUGFIX] SelectBig

--- src/pages/person/PersonPage.jsx
+++ src/pages/person/PersonPage.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import SelectBig from "./components/basics/select-big/SelectBig";
+import { Input } from "../../components/basic/input/Input";
 
 export default function PersonPage() {
-  return <div>PersonPage</div>;
+  return <div>PersonPage
+    <SelectBig/>
+    <Input/>
+  </div>;
 }
