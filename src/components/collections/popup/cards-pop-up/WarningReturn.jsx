14ade95324e417f955d4c327e153c11dd9bb12aa
Mariatroyano <maria2020troyano@gmail.com>
2024-12-06 12:21:36 -0500

    [FEATURE add WeCanGenerateTryLater]

---
+++ src/components/collections/popup/cards-pop-up/WarningReturn.jsx
@@ -0,0 +1,27 @@
+// bhbhb
+
+import React from "react";
+import PopupMain from "../PopupMain";
+import IconDanger from "../../../../assets/icons/svg/Icon-danger.svg";
+export default function WarningReturn({ btn_misNegocios, btn_reintentar }) {
+  return (
+    <PopupMain
+      img={IconDanger}
+      title={<span style={{ color: "#1A1446" }}>Apreciado intermediario</span>}
+      buttons={[
+        {
+          typeStyle: "primary",
+          text: "Si,regresar",
+          action: btn_misNegocios,
+        },
+        {
+          typeStyle: "secondary",
+          text: "No,continuar",
+          action: btn_reintentar,
+        },
+      ]}
+      text1={"Ten en cuenta que una vez regreses a pasos anteriores de la cotización desde este punto, es posible que se generen variaciones en la tarifa entregada inicialmente"}
+      text2={""}
+    />
+  );
+}
--- src/routes/configRouter.jsx
+++ src/routes/configRouter.jsx
@@ -1,4 +1,6 @@
 import { PanelInformation } from "../components/collections/panel-box/PanelInformation";
+import WarningReturn from "../components/collections/popup/cards-pop-up/WarningReturn";
+
 import PersonPage from "../pages/person/PersonPage";
 import QuotationPage from "../pages/quotation/QuotationPage";
 import CardPlan from "../pages/quotation/basic/card-plan/CardPlan"
@@ -7,5 +9,7 @@ export const routes = [
   { path: "/", element: <PersonPage /> },
   { path: "/quotation", element: <QuotationPage /> },
   { path: "/CardPlan", element: <CardPlan/> },
+  { path: "/WarningReturn", element: <WarningReturn/> },
+ 
   
 ];
